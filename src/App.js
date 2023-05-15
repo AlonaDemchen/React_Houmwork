c5b15f64e810e8e4e0fd3750611a0fb50e208871
username <usermail>
2023-05-15 17:49:34 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import CardItem from './components/CardItem';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <img class="logo" src="https://brammels.com/wp-content/uploads/2022/10/a-level-ukraine-logo.png" alt="Oops!" />
+        <nav class="main-nav hamburger-menu">
+          <input id="menu__toggle" type="checkbox" />
+          <label class="menu__btn" for="menu__toggle">
+            <span></span>
+          </label>
+
+          <ul class="menu__box">
+            <li><a class="menu__item" href="#">Головна сторінка</a></li>
+            <li><a class="menu__item" href="#">Наша команда</a></li>
+            <li><a class="menu__item" href="#">Наші роботи</a></li>
+            <li><a class="menu__item" href="#">Блог</a></li>
+            <li><a class="menu__item" href="#">Контакти</a></li>
+          </ul>
+        </nav>
+
       </header>
-    </div>
+      <main>
+        <aside class="aside1">
+          <p> Aside 1</p>
+        </aside>
+        <div class="text">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium ab obcaecati quos odio rem quo, vel molestiae consectetur facere facilis saepe et nemo itaque. Expedita aut illum sint sed ipsa optio reprehenderit suscipit ducimus unde, aspernatur corporis cum pariatur explicabo. Voluptate fugit sint, exercitationem illum ad aut aperiam deleniti delectus.</p>
+          <h1>OUR WORK</h1>
+          <div class="card_items">
+            <CardItem/>
+            <CardItem/>
+            <CardItem/>
+            <CardItem/>
+            <CardItem/>
+            <CardItem/>
+          </div>
+        </div>
+        <aside class="aside2">
+          <p> Aside 2</p>
+        </aside>
+
+      </main>
+      <footer>
+        <p>Footer</p>
+      </footer>
+    </>
   );
 }
 
---
+++ src/components/CardItem.jsx
@@ -0,0 +1,13 @@
+import '../App.scss';
+
+
+const CardItem =()=>{return(<div class="card_item">
+<img class="img_in_icons" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTyeoP6x4XnXs3YssRO4_P6EG4EmQkRLUJ7tcRcIOP9qr3wveE9mkyTgZtgH_SLJE18HM&usqp=CAU" alt="Oops!" />
+<h3>Lorem, ipsum dolor.</h3>
+<p class="text_icons">
+  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis odit reprehenderit, illum minus optio est at odio veniam rem ab!
+</p>
+<p class="Go">Go</p>
+</div>)}
+
+export default CardItem;
